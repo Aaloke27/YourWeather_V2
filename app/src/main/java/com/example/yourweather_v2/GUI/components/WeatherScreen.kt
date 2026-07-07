8e050641b865451605c14faa36a5daa259f6baf6
Aaloke Das <aalokedas07@gmail.com>
2026-07-07 17:26:03 +0530

    Update current data cards layout

--- app/src/main/java/com/example/yourweather_v2/GUI/components/WeatherScreen.kt
+++ app/src/main/java/com/example/yourweather_v2/GUI/components/WeatherScreen.kt
@@ -5,15 +5,18 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -36,14 +39,23 @@ fun WeatherScreen(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 16.dp)
-                .fillMaxHeight(0.3f)
+                .fillMaxHeight()
                 .padding(top = 18.dp)
+                .align(Alignment.CenterHorizontally)
         ) {
+            Column() {
+                Spacer(modifier = Modifier.height(12.dp))
             LocationCard()
+            Spacer(modifier = Modifier.height(12.dp))
+            CurrentWeatherSection(weather)
+                Spacer(modifier = Modifier.height(20.dp))
+                CurrentDataCards(weather)
+             }
         }
 
     }
 
 }
+
 
 
